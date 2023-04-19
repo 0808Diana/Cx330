6078521c23059d727a60df2b90fddaec8d002332
0808Diana <327850390@qq.com>
2023-04-19 22:30:08 +0800

    计算圆的周长和面积4/19

--- 计算圆的周长和面积.cpp
+++ 计算圆的周长和面积.cpp
@@ -9,12 +9,17 @@ double Square(double R)
 {
 	return R*R*3.14;
 }
+double D(double R)
+{
+	return 2*R;
+}
 
 int main()
 {
 	double n;
 	cin>>n;
-	cout<<Circumference(n)<<endl;
-	cout<<Square(n)<<endl;
+	cout<<"直径="<<D(n)<<endl;
+	cout<<"周长="<<Circumference(n)<<endl;
+	cout<<"面积="<<Square(n)<<endl;
 	return 0;
 }
